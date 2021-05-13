53e50177e49c846ee5a14cc73be6a048ebf5c4cb
zBeppe123 <capodacquapiostefano@outlook.it>
2021-05-13 11:51:49 +0200

    Update kk.java

--- kk.java
+++ kk.java
@@ -2,8 +2,11 @@ public class kk {
 	public static void main(String[] args) {
 		System.out.println("snfskjnfkjs<bfjkbzs")
 	}
-	
+	public void mimmo(){
+		return;
+	}
 	public void nulla() {
 		int h = 0;
+		int g=1;
 	}
 }
