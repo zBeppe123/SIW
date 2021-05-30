4241cbeffb8bcc18d055fb6c3c90424c0186ce9c
Botta Michele <72565868+bottamichele@users.noreply.github.com>
2021-05-31 00:36:42 +0200

    Fix sulla richiesta "/tennista/{id}"
    - Risolto il problema sulla richiesta "/tennista/{id}" che non la trovava

--- TorneiTennis/src/main/java/it/uniroma3/siw/tennis/spring/controller/TennistaController.java
+++ TorneiTennis/src/main/java/it/uniroma3/siw/tennis/spring/controller/TennistaController.java
@@ -1,7 +1,7 @@
 package it.uniroma3.siw.tennis.spring.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +12,7 @@ import it.uniroma3.siw.tennis.spring.model.Tennista;
 import it.uniroma3.siw.tennis.spring.service.PartitaService;
 import it.uniroma3.siw.tennis.spring.service.TennistaService;
 
-@Component
+@Controller
 public class TennistaController {
 	@Autowired
 	private TennistaService tennistaService;
@@ -22,7 +22,7 @@ public class TennistaController {
 	private TennistaValidator tennistaValidator;
 
 	@RequestMapping(value = "/tennista/{id}", method = RequestMethod.GET)
-	public String getArbitro(@PathVariable("id") Long idTennista, Model model) {
+	public String getTennista(@PathVariable("id") Long idTennista, Model model) {
 		Tennista t = this.tennistaService.tennistaPerId(idTennista);
 		if (t != null) {
 			model.addAttribute("tennista", this.tennistaService.tennistaPerId(idTennista));
