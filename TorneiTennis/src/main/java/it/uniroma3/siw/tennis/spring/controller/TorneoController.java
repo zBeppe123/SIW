be7c169817442787ba8b886ab1669657867b2541
zBeppe123 <capodacquapiostefano@outlook.it>
2021-06-06 18:10:10 +0200

    fix minori

--- TorneiTennis/src/main/java/it/uniroma3/siw/tennis/spring/controller/TorneoController.java
+++ TorneiTennis/src/main/java/it/uniroma3/siw/tennis/spring/controller/TorneoController.java
@@ -1,5 +1,6 @@
 package it.uniroma3.siw.tennis.spring.controller;
 
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
--- TorneiTennis/src/main/java/it/uniroma3/siw/tennis/spring/controller/validator/PartitaValidator.java
+++ TorneiTennis/src/main/java/it/uniroma3/siw/tennis/spring/controller/validator/PartitaValidator.java
@@ -1,15 +1,8 @@
 package it.uniroma3.siw.tennis.spring.controller.validator;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
-import org.springframework.validation.ValidationUtils;
-import org.springframework.validation.Validator;
 
-import it.uniroma3.siw.tennis.spring.model.Partita;
-import it.uniroma3.siw.tennis.spring.service.PartitaService;
 
 @Component
 public class PartitaValidator{
