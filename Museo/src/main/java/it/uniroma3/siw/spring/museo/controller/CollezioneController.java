61ea29a5d7ea1a879feb0890219670d8db0d7685
zBeppe123 <capodacquapiostefano@outlook.it>
2021-06-11 23:22:18 +0200

    modifiche a collezione controller e service

--- Museo/src/main/java/it/uniroma3/siw/spring/museo/controller/CollezioneController.java
+++ Museo/src/main/java/it/uniroma3/siw/spring/museo/controller/CollezioneController.java
@@ -1,5 +1,30 @@
 package it.uniroma3.siw.spring.museo.controller;
 
-public class CollezioneController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import it.uniroma3.siw.spring.museo.controller.validator.CollezioneValidator;
+
+import it.uniroma3.siw.spring.museo.model.Collezione;
+import it.uniroma3.siw.spring.museo.service.CollezioneService;
+
+@Controller
+public class CollezioneController {
+	@Autowired
+	private CollezioneService collezioneService;
+	@Autowired
+	private CollezioneValidator collezioneValidator;
+	
+	@RequestMapping(value = "/collezione/{id}", method = RequestMethod.GET)
+	public String getArtista(@PathVariable("id") Long idCollezione, Model model) {
+		Collezione c=this.collezioneService.collezionePerId(idCollezione);
+		if(c!=null) {
+			model.addAttribute("collezione",c);
+		}
+		return "collezione";
+	}
 }
