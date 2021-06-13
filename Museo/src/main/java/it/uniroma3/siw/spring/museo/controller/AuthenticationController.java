a8040dcd991fe9e3b3b4b0a2e21087b4c86763fc
zBeppe123 <72344861+zBeppe123@users.noreply.github.com>
2021-06-13 08:41:51 +0200

    Aggiunti commenti

--- Museo/src/main/java/it/uniroma3/siw/spring/museo/controller/AuthenticationController.java
+++ Museo/src/main/java/it/uniroma3/siw/spring/museo/controller/AuthenticationController.java
@@ -15,17 +15,38 @@ import it.uniroma3.siw.spring.museo.service.CredenzialiService;
 public class AuthenticationController {
 	@Autowired
 	private CredenzialiService credenzialiService;
-	
+	/**
+	 * Questa funzione apre la pagina del login
+	 * @param model
+	 * @param artista
+	 * @param bindingResult
+	 * @return stringa riferita alla pagina login.html
+	 */
+
 	@RequestMapping(value = "/login", method = RequestMethod.GET) 
 	public String apriPaginaLogin(Model model) {
 		return "login.html";
 	}
-	
+	/**
+	 * questa pagina fa tornare l'utente all'index dopo aver premuto il tasto logout
+	 * @param model
+	 * @param artista
+	 * @param bindingResult
+	 * @return stringa riferita a index.html
+	 */
+
 	@RequestMapping(value = "/logout", method = RequestMethod.GET) 
 	public String logout(Model model) {
 		return "index.html";
 	}
-	
+	/**
+	 * questa funzione porta alla home dell'utente entrato
+	 * @param model
+	 * @param artista
+	 * @param bindingResult
+	 * @return stringa admin/home se il ruolo dell'utente è admin altrimenti la stringa home per utenti normali
+	 */
+
 	@RequestMapping(value = "/default", method = RequestMethod.GET)
     public String defaultAfterLogin(Model model) {
     	UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
